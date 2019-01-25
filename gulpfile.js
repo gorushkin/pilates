f44d8de33ac27e045a2967c99ca5d864e5bf19d3
gorushkin <gorushkin@gmail.com>
2019-01-25 15:39:43 +0300

    автоматизация_1

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,38 @@
-function defaultTask(cb) {
-  // place code for your default task here
-  cb();
-}
+"use strict";
 
-exports.default = defaultTask
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var del = require('del');
+var server = require("browser-sync").create();
+
+
+gulp.task('css', function () {
+  return gulp.src('source/sass/style.scss')
+  .pipe(sass())
+  .pipe(gulp.dest("build/css"))
+});
+
+gulp.task('html', function() {
+  return gulp.src('source/*.html', {
+    base: 'source'
+  })
+  .pipe(gulp.dest('build'))
+});
+
+gulp.task('clean', function() {
+  return del("build")
+});
+
+gulp.task('build', gulp.series (
+  'clean',
+  'css',
+  'html'
+));
+
+gulp.task('qwe', function(){
+  console.log('seen');
+});
+
+gulp.task('watch', function(){
+  gulp.watch('source/*.html', gulp.series('build'));
+});
